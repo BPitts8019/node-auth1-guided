edc18a5dd1541c7694669edde6d5d98285e9b72e
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-14 18:39:36 -0700

    added password hash with time constraint

--- users/users-model.js
+++ users/users-model.js
@@ -1,32 +1,35 @@
-const db = require("../database/dbConfig")
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
 
 function find() {
-  return db("users")
-    .select("id", "username")
+   return db("users")
+      .select("id", "username")
 }
 
 function findBy(filter) {
-  return db("users")
-    .where(filter)
-    .select("id", "username", "password")
+   return db("users")
+      .where(filter)
+      .select("id", "username", "password")
 }
 
 async function add(user) {
-  const [id] = await db("users")
-    .insert(user)
- 
-  return findById(id)
+   user.password = await bcrypt.hash(user.password, 14);
+
+   const [id] = await db("users")
+      .insert(user)
+
+   return findById(id)
 }
 
 function findById(id) {
-  return db("users")
-    .where({ id })
-    .first("id", "username")
+   return db("users")
+      .where({ id })
+      .first("id", "username")
 }
 
 module.exports = {
-  add,
-  find,
-  findBy,
-  findById,
+   add,
+   find,
+   findBy,
+   findById,
 }
