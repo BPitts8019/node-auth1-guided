6c5dce3843ce976c4e3da2b27c2d046cf06acca1
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-14 18:56:07 -0700

    Considers the password entered for authorization

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,35 +1,37 @@
 const express = require("express")
+const bcrypt = require("bcryptjs");
 const usersModel = require("../users/users-model")
 
 const router = express.Router()
 
 router.post("/register", async (req, res, next) => {
-  try {
-    const saved = await usersModel.add(req.body)
-    
-    res.status(201).json(saved)
-  } catch (err) {
-    next(err)
-  }
+   try {
+      const saved = await usersModel.add(req.body)
+
+      res.status(201).json(saved)
+   } catch (err) {
+      next(err)
+   }
 })
 
 router.post("/login", async (req, res, next) => {
-  try {
-    const { username, password } = req.body
-    const user = await usersModel.findBy({ username }).first()
+   try {
+      const { username, password } = req.body
+      const user = await usersModel.findBy({ username }).first()
+      const passwordValid = await bcrypt.compare(password, user.password);
 
-    if (user) {
-      res.status(200).json({
-        message: `Welcome ${user.username}!`,
-      })
-    } else {
-      res.status(401).json({
-        message: "Invalid Credentials",
-      })
-    }
-  } catch (err) {
-    next(err)
-  }
+      if (user && passwordValid) {
+         res.status(200).json({
+            message: `Welcome ${user.username}!`,
+         })
+      } else {
+         res.status(401).json({
+            message: "Invalid Credentials",
+         })
+      }
+   } catch (err) {
+      next(err)
+   }
 })
 
 module.exports = router
